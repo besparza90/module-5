d3e9a927d785ca26e81c8264c339028afc6dbbf2
besparza <esparzabrian90@gmail.com>
2021-12-03 00:24:15 -0800

    added timer and rows

---
+++ script.js
@@ -0,0 +1,8 @@
+// get a new date (locale machine date time)
+var date = new Date();
+// get the date as a string
+var n = date.toDateString();
+
+// find the html element with the id of time
+// set the innerHTML of that element to the date a space the time
+document.getElementById('currentDay').innerHTML = n
