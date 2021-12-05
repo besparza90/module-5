d9093a547535aced270de38f800f08be01ec58c8
besparza <esparzabrian90@gmail.com>
2021-12-05 15:10:36 -0800

    added color coding

--- script.js
+++ script.js
@@ -8,14 +8,32 @@ $(function () {
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
     })
+
 //Allows input to be saved.
     $("#time9 .description").val(localStorage.getItem("time9"));
     $("#time10 .description").val(localStorage.getItem("time10"));
     $("#time11 .description").val(localStorage.getItem("time11"));
     $("#time12 .description").val(localStorage.getItem("time12"));
-    $("#time1 .description").val(localStorage.getItem("time1"));
-    $("#time2 .description").val(localStorage.getItem("time2"));
-    $("#time3 .description").val(localStorage.getItem("time3"));
-    $("#time4 .description").val(localStorage.getItem("time4"));
-    $("#time5 .description").val(localStorage.getItem("time5"));
-}) 
+    $("#time13 .description").val(localStorage.getItem("time13"));
+    $("#time14 .description").val(localStorage.getItem("time14"));
+    $("#time15 .description").val(localStorage.getItem("time15"));
+    $("#time16 .description").val(localStorage.getItem("time16"));
+    $("#time17 .description").val(localStorage.getItem("time17"));
+    
+    colorHour();
+})
+//The function retreives the current hour and creates a loop where each block goes through a parse function. The block is colored based on the relation between the value and the current time. This is done by adding CSS classes.
+function colorHour() {
+    var currentTime = moment().hour();
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("time")[1]);
+        if (blockTime < currentTime) {
+            $(this).addClass("past");
+        } else if (blockTime === currentTime) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+
+        }
+    })
+}
