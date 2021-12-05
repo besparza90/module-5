d5d76e42d4c283edc96c875c0587cf6d0589ac32
besparza <esparzabrian90@gmail.com>
2021-12-05 12:24:40 -0800

    added input storage

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-// get a new date (locale machine date time)
-var date = new Date();
-// get the date as a string
-var n = date.toDateString();
-
-// find the html element with the id of time
-// set the innerHTML of that element to the date a space the time
-document.getElementById('currentDay').innerHTML = n
+//This function generates the current date. It is generated in the header with .html into the "currentDay" id.
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+//Ready() is used to store other functions. .on is used as an event handler for the save button. The functions sets variables and stores elements from user input.
+$(function () { 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+//Allows input to be saved.
+    $("#time9 .description").val(localStorage.getItem("time9"));
+    $("#time10 .description").val(localStorage.getItem("time10"));
+    $("#time11 .description").val(localStorage.getItem("time11"));
+    $("#time12 .description").val(localStorage.getItem("time12"));
+    $("#time1 .description").val(localStorage.getItem("time1"));
+    $("#time2 .description").val(localStorage.getItem("time2"));
+    $("#time3 .description").val(localStorage.getItem("time3"));
+    $("#time4 .description").val(localStorage.getItem("time4"));
+    $("#time5 .description").val(localStorage.getItem("time5"));
+}) 
